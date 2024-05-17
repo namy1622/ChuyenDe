eedd65c9459d0aa61fa938e52efb2f6ad1b8be06
Đoàn Nam <100189938+namy1622@users.noreply.github.com>
2024-05-18 03:36:37 +0700

    Update getPass.php

--- BTL_ChuyenDe/Project/getPass.php
+++ BTL_ChuyenDe/Project/getPass.php
@@ -1,11 +1,35 @@
-<<<<<<< HEAD
 <?php
 session_start();
-echo isset($_SESSION['pass_word']) ? $_SESSION['pass_word'] : '';
+
+require "config.php";
+
+$email = isset($_POST['email']) ? $_POST['email'] : '';
+$verificationCode_test = isset($_POST['verificationCode']) ? $_POST['verificationCode'] : '';
+
+// if ($verificationCode_test) {
+    $code = isset($_POST['verificationCode']) ? $_POST['verificationCode'] : '';
+    $storedCode = $_SESSION["verification_code_for_$email"];
+    
+    if ($storedCode === $code) {
+        // khi đã so sánh xong thì xóa code vừa tạo đi
+        unset($_SESSION["verification_code_for_$email"]);
+
+         // Thực hiện truy vấn SQL
+       $sql = "SELECT pass_word FROM login WHERE user_name = '$email'";
+       $result = mysqli_query($conn, $sql);
+   
+        // lưu Pass vào $_SESSION['pass_word'], chuyển qua getPass để gọi lại nó
+        //.... gọi tại đây dính mã html
+       $_SESSION['pass_word'] = mysqli_fetch_assoc($result)['pass_word'];
+      
+    //    header("Location: /send.php", true, 200 );
+
+       echo isset($_SESSION['pass_word']) ? $_SESSION['pass_word'] : '';
+    } 
+    else {
+        header("Location: /send.php", true, 500 );
+    }
+// }
+
+//echo isset($_SESSION['pass_word']) ? $_SESSION['pass_word'] : '';
 ?>
-=======
-<?php
-session_start();
-echo isset($_SESSION['pass_word']) ? $_SESSION['pass_word'] : '';
-?>
->>>>>>> de949d9b7fd80dc176d99a41c7ee99e393b3b3aa
